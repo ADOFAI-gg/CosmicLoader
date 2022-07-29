87ddfef22de9e4ca042db006561b102b0717fe31
C#Newbie <73321185+c3nb@users.noreply.github.com>
2022-07-30 00:57:55 +0900

    초기 커밋.

---
+++ AdofaiModManager/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace AdofaiModManager
+{
+    public class Library
+    {
+        public string FileName;
+        public Library[] References;
+        public void Resolve(string modPath)
+        {
+            for (int i = 0; i < References?.Length; i++)
+                References[i].Resolve(modPath);
+            AppDomain.CurrentDomain.Load(File.ReadAllBytes($"{modPath}/{FileName}"));
+        }
+    }
+}
---
+++ AdofaiModManager/ManagerObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.IO;
+
+namespace AdofaiModManager
+{
+    public class ManagerObject : MonoBehaviour
+    {
+        public void Awake()
+        {
+            DontDestroyOnLoad(this);
+            Application.quitting += () =>
+            {
+                File.WriteAllText(ManagerConfig.Path, ModManager.Config.ToJson());
+                foreach (Mod mod in ModManager.Mods)
+                {
+                    if (!mod.Active) continue;
+                    try { mod.OnExit?.Invoke(); }
+                    catch (Exception e) { mod.Logger.LogException("OnExit", e); }
+                }
+            };
+        }
+        public void Update()
+        {
+            foreach (Mod mod in ModManager.Mods)
+            {
+                if (!mod.Active) continue;
+                try { mod.OnUpdate?.Invoke(); }
+                catch (Exception e) { mod.Logger.LogException("OnUpdate", e); }
+            }
+        }
+        public void OnGUI()
+        {
+            if (GUILayout.Button("Open Log"))
+                ModManager.OpenUnityFileLog();
+        }
+    }
+}
