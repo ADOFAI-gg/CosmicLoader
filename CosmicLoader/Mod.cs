6573d224e16e611bb01986b50f9d06412665a3ec
papertoy1127 <richard1909@naver.com>
2022-08-06 20:45:59 +0900

    Formatting fix

--- CosmicLoader/Mod.cs
+++ CosmicLoader/Mod.cs
@@ -7,8 +7,10 @@ using CosmicLoader.UMM;
 using UnityEngine;
 using UnityModManagerNet;
 
-namespace CosmicLoader {
-    public class Mod {
+namespace CosmicLoader
+{
+    public class Mod
+    {
         public ModInfo Info { get; private set; }
         public ModLogger Logger { get; private set; }
         public Harmony Harmony { get; private set; }
@@ -20,7 +22,9 @@ namespace CosmicLoader {
         public Action OnUpdate;
         public Action OnExit;
         private bool active;
-        internal Mod(ModInfo mInfo) {
+
+        internal Mod(ModInfo mInfo)
+        {
             Info = mInfo;
             Harmony = new Harmony(mInfo.Id);
             Path = mInfo.Path;
@@ -31,9 +35,11 @@ namespace CosmicLoader {
             dict[mInfo.Id] = false;
         }
 
-        public bool Active {
+        public bool Active
+        {
             get => active;
-            set {
+            set
+            {
                 active = value;
                 if (active)
                     TryLoad();
@@ -41,16 +47,21 @@ namespace CosmicLoader {
             }
         }
 
-        public bool TryLoad() {
-            if (Loaded) {
+        public bool TryLoad()
+        {
+            if (Loaded)
+            {
                 LoadFailed = false;
                 return true;
             }
 
-            try {
+            try
+            {
                 if (Info.IsUMM) LoadUMM();
                 else Load();
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Logger.LogException($"Error while loading mod {Info.Id}", e);
                 LoadFailed = true;
                 return false;
@@ -60,8 +71,10 @@ namespace CosmicLoader {
             return true;
         }
 
-        void Load() {
-            static MethodInfo GetEntryPoint(Assembly assembly, string entry) {
+        void Load()
+        {
+            static MethodInfo GetEntryPoint(Assembly assembly, string entry)
+            {
                 var identifiers = entry.Split('.');
                 var methodName = identifiers[identifiers.Length - 1];
                 var typeName = string.Join(".", identifiers.Take(identifiers.Length - 1));
@@ -70,23 +83,28 @@ namespace CosmicLoader {
             }
 
             var dllPath = System.IO.Path.Combine(Path, Info.FileName);
-            if (!File.Exists(dllPath)) {
+            if (!File.Exists(dllPath))
+            {
                 throw new Exception($"Mod {Info.Id} file not found!");
             }
 
             var assembly = Assembly.LoadFile(dllPath);
             var entryMethod = GetEntryPoint(assembly, Info.EntryPoint);
-            if (entryMethod == null) {
+            if (entryMethod == null)
+            {
                 throw new Exception($"Mod {Info.Name} entry not found!");
             }
 
             this.Assembly = entryMethod.DeclaringType!.Assembly;
-            var res = entryMethod.Invoke(null, new object[] { this });
+            var res = entryMethod.Invoke(null, new object[] {this});
             if (res is false) throw new Exception($"Mod {Info.Name} entry method returned false!");
             Loaded = true;
         }
-        void LoadUMM() {
-            static MethodInfo GetEntryPoint(Assembly assembly, string entry) {
+
+        void LoadUMM()
+        {
+            static MethodInfo GetEntryPoint(Assembly assembly, string entry)
+            {
                 var identifiers = entry.Split('.');
                 var methodName = identifiers[identifiers.Length - 1];
                 var typeName = string.Join(".", identifiers.Take(identifiers.Length - 1));
@@ -95,31 +113,37 @@ namespace CosmicLoader {
             }
 
             var dllPath = System.IO.Path.Combine(Path, Info.FileName);
-            if (!File.Exists(dllPath)) {
+            if (!File.Exists(dllPath))
+            {
                 throw new Exception($"Mod {Info.Id} file not found!");
             }
+
             var assembly = Assembly.LoadFile(dllPath);
             var entryMethod = GetEntryPoint(assembly, Info.EntryPoint);
-            if (entryMethod == null) {
+            if (entryMethod == null)
+            {
                 throw new Exception($"Mod {Info.Name} entry not found!");
             }
 
             this.Assembly = entryMethod.DeclaringType!.Assembly;
             var entry = Integration.CreateModEntry(this);
             Debug.Log($"Created mod entry {entry} for {Info.Name} assembly {entry.Assembly}");
-            var res = entryMethod.Invoke(null, new object[] { entry });
+            var res = entryMethod.Invoke(null, new object[] {entry});
             if (res is false) throw new Exception($"Mod {Info.Name} entry method returned false!");
             _entry = entry;
             Loaded = true;
         }
 
-        public Action OnGUI {
-            get {
+        public Action OnGUI
+        {
+            get
+            {
                 if (Info.IsUMM) return () => _entry.OnGUI(_entry);
                 return _onGUI;
             }
 
-            set {
+            set
+            {
                 _onGUI = value;
             }
         }
